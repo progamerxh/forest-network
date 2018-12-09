9602335e3b261d8f21daf645c747ffb02834b5f7
Ku Hiệp <vipmrak97h@gmail.com>
2018-12-09 23:18:52 +0700

    Keypair save

--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -2,6 +2,17 @@ import React, { Component } from 'react';
 import { login } from '../Actions/AuthAction'
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom'
+import { Keypair } from 'stellar-base'
+
+// Email: xuanhiep28697@gmail.com
+// publicKey: GAMKCQ2STC3NLPEHMYKQCT7MPBRB33YTQ2SJGDTYY3245QLREJX3VXHP
+// secretKey: SCN2JJCHUKNEHMPE5W6KWGMVB6LCKKMLD7JLQQAOJDPB2NYALHHFG6JC
+// Email: vipmrak97h@gmail.com
+// publicKey: GA3GUNBIZ3CPADC53GMM24TKF7B7ILTJZ3VOXBAUSPM7ILPUZ2ECTRE6
+// secretKey: SAD6LNC7KB56TZILDBE63XVF4VVLN3VDXRJUUNY7YJEYCZJL4GJO6QQM
+// Email: 1512169@student.hcmus.edu.vn
+// publicKey: GAJDT2HYYIBPAPSTCCUETUXCCFBG6NB62O43MXLD55MP76T7IDPE5QR7
+// secretKey: SCQ4BCKFWK3KB62F45RQCWLOF5VSDFG4BC3JCOKDBTP5FZFXYJTAPL2Y
 
 function SignIn(props) {
     const auth = {
@@ -11,21 +22,21 @@ function SignIn(props) {
         infor: "Forest Network là một mạng xã hội phi tập trung (decentralized social network) dựa trên công nghệ chuỗi khối (blockchain technology)",
         followers: [
             {
-                uid :"u1",
+                uid: "u1",
                 avatarUrl: "http://droidlessons.com/wp-content/uploads/2016/11/Cogn_mode.png",
                 displayName: "User 1",
                 followers: "112",
                 isFollow: true,
             },
             {
-                uid :"u2",
+                uid: "u2",
                 avatarUrl: "http://droidlessons.com/wp-content/uploads/2016/11/Cogn_mode.png",
                 displayName: "User 2",
                 followers: "13",
                 isFollow: false,
             },
             {
-                uid :"u3",
+                uid: "u3",
                 avatarUrl: "http://droidlessons.com/wp-content/uploads/2016/11/Cogn_mode.png",
                 displayName: "User 3",
                 followers: "14",
@@ -34,13 +45,13 @@ function SignIn(props) {
         ],
         following: [
             {
-                uid :"u1",
+                uid: "u1",
                 avatarUrl: "http://droidlessons.com/wp-content/uploads/2016/11/Cogn_mode.png",
                 displayName: "User 1",
                 followers: "112",
             },
             {
-                uid :"u7",
+                uid: "u7",
                 avatarUrl: "http://droidlessons.com/wp-content/uploads/2016/11/Cogn_mode.png",
                 displayName: "User 7",
                 followers: "42",
@@ -55,8 +66,14 @@ function SignIn(props) {
                 <h2>Welcome to</h2>
                 <h1>Forest<span>Network</span>  </h1>
                 <p> Forest Network  is a decentralized social network<br></br> based on blockchain technology</p>
-                <div class="interaction">
-                    <div className="register">TO REGISTER</div>
+                <div className="interaction">
+                    <div className="register"
+                        onClick={() => {
+                            const key = Keypair.random();
+                            console.log(key.secret());
+                            console.log(key.publicKey());
+                        }}
+                    >TO REGISTER</div>
                     <div className="login"
                         onClick={() => { props.dispatch(login(auth)) }}
                     >Login</div>
